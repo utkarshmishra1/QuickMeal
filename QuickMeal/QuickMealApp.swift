4758f38fa2fdc14ca7e4305c677039a77868cf8e
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-09-04 00:38:58 +0530

    Initial Commit

---
+++ QuickMeal/QuickMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMealApp.swift
+//  QuickMeal
+//
+//  Created by utkarsh mishra on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
